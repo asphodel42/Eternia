c0dee351d582f481802fa64e92e545512689cf34
asphodel42 <asphodell42@gmail.com>
2024-11-17 18:03:43 +0200

    added function for send button

--- static/js/chat.js
+++ static/js/chat.js
@@ -11,3 +11,40 @@ document.querySelector('.search input').addEventListener('input', function() {
         }
     });
 });
+
+document.getElementById('sendButton').addEventListener('click', function() {
+    var messageInput = document.getElementById('messageInput');
+    var messageText = messageInput.value.trim();
+
+    if (messageText) {
+        // Отримуємо chat_id з data-атрибуту елемента #chat-data
+        var chatDataElement = document.getElementById('chat-data');
+        var selectedChatId = chatDataElement ? parseInt(chatDataElement.getAttribute('data-chat-id'), 10) : null;
+
+        // Перевірка, чи є chat_id
+        if (selectedChatId) {
+            // Відправка POST-запиту до сервера
+            fetch('/send_message', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    chat_id: selectedChatId,
+                    message: messageText,
+                }),
+            })
+            .then(response => {
+                if (response.ok) {
+                    messageInput.value = '';
+                    location.reload();
+                } else {
+                    alert('Failed to send the message. Please try again.');
+                }
+            })
+            .catch(error => console.error('Error:', error));
+        } else {
+            alert('Chat ID is missing!');
+        }
+    }
+});
